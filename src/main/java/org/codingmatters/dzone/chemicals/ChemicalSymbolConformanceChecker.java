73094869b54109bcc28b7cfded4b40c3e3d67c38
nelt <nel.taurisson@gmail.com>
2016-08-09 06:32:11 +0000

    rule 1 to 3

--- src/main/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceChecker.java
+++ src/main/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceChecker.java
@@ -5,4 +5,26 @@ package org.codingmatters.dzone.chemicals;
  */
 public class ChemicalSymbolConformanceChecker {
 
+    public boolean isConform(String element, String symbol) {
+        element = element.toLowerCase();
+        symbol = symbol.toLowerCase();
+        if(symbol.length() != 2) {
+            return false;
+        }
+        for (char c : symbol.toCharArray()) {
+            if(element.indexOf(c) == -1) {
+                return false;
+            }
+        }
+
+        int firstIndex = element.indexOf(symbol.charAt(0));
+        String remaining = element.substring(firstIndex + 1);
+        int secondIndex = firstIndex + 1 + remaining.indexOf(symbol.charAt(1));
+
+        if(firstIndex >= secondIndex) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/test/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceCheckerTest.java
+++ src/test/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceCheckerTest.java
@@ -1,10 +1,46 @@
 package org.codingmatters.dzone.chemicals;
 
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
  * Created by nelt on 8/9/16.
  */
 public class ChemicalSymbolConformanceCheckerTest {
+
+    private ChemicalSymbolConformanceChecker checker = new ChemicalSymbolConformanceChecker();
+
+    @Test
+    public void conform() throws Exception {
+        assertThat(checker.isConform("Boron", "Bo"), is(true));
+    }
+
+    @Test
+    public void lessThanTwoLetters_NotConform() throws Exception {
+        assertThat(checker.isConform("Boron", "B"), is(false));
+    }
+
+    @Test
+    public void moreThanTwoLetters_NotConform() throws Exception {
+        assertThat(checker.isConform("Boron", "Bor"), is(false));
+    }
+
+    @Test
+    public void firstLetterNotInElement_NotConform() throws Exception {
+        assertThat(checker.isConform("Boron", "Zo"), is(false));
+    }
+
+    @Test
+    public void secondLetterNotInElement_NotConform() throws Exception {
+        assertThat(checker.isConform("Boron", "Bz"), is(false));
+    }
+
+    @Test
+    public void symbolCharacterNotInElementOrder_NotConform() throws Exception {
+        assertThat(checker.isConform("Silver", "Rv"), is(false));
+    }
 
 }
