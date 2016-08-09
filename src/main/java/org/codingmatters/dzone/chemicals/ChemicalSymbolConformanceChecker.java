09f524e36cdaccd9cc61298cb28492e97f4eaa03
nelt <nel.taurisson@gmail.com>
2016-08-09 06:12:21 +0000

    init conformance checker

---
+++ src/main/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceChecker.java
@@ -0,0 +1,8 @@
+package org.codingmatters.dzone.chemicals;
+
+/**
+ * Created by nelt on 8/9/16.
+ */
+public class ChemicalSymbolConformanceChecker {
+
+}
---
+++ src/test/java/org/codingmatters/dzone/chemicals/ChemicalSymbolConformanceCheckerTest.java
@@ -0,0 +1,10 @@
+package org.codingmatters.dzone.chemicals;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nelt on 8/9/16.
+ */
+public class ChemicalSymbolConformanceCheckerTest {
+
+}
